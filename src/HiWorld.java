e5c3d7bcd5e9b7d44b7333d77ce69d34d8471b9b
박재성 <parkjaesung@solutis.co.kr>
2020-07-06 10:00:17 +0900

    새로운 테스트 java파

---
+++ src/HiWorld.java
@@ -0,0 +1,6 @@
+
+public class HiWorld {
+	public static void main(String[] aaaa) {
+		System.out.println("이것은 hiworld 테스트 입니다.");
+	}
+}
--- src/hi.java
+++ src/hi.java
@@ -1,1 +1,9 @@
 
+//프로젝트 명과 같은 클래스명이어야 한다.
+public class hi {
+	public static void main(String[] args) {
+		System.out.println("wefwefwef");
+		System.out.println("이것은 테스트");
+		System.out.println("이것은 첫번째 테스");
+	}
+}
